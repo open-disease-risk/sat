9a34cd80f4c1ce55977fcbf7e5ff3383bc471a35
dahlem <dominik.dahlem@gmail.com>
2025-03-22 17:00:40 +0000

    added observation-based ranking of events

---
+++ sat/loss/ranking/__init__.py
@@ -0,0 +1,8 @@
+"""Ranking loss functions for survival analysis"""
+
+__authors__ = ["Dominik Dahlem", "Mahed Abroshan"]
+__status__ = "Development"
+
+from .multievent import MultiEventRankingLoss  # noqa
+from .sample import SampleRankingLoss  # noqa
+from .observation import ObservationEventRankingLoss  # noqa
