6e7beb6266063086ee91aa3c8301037f81e932a7
Dominik Dahlem <dominik.dahlem@gmail.com>
2025-06-12 04:34:06 +0100

    Update sat/finetune.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- sat/finetune.py
+++ sat/finetune.py
@@ -78,7 +78,7 @@ def _finetune(cfg: DictConfig) -> pd.DataFrame:
         logger.info(
             f"Splitting dataset with k-fold configuration: k={cfg.cv.k}; fold={cfg.replication}"
         )
-        fold_index = cfg.cv.k if cfg.cv.k else None
+        fold_index = cfg.cv.k or None
         ds_splitter = splitter.StreamingKFoldSplitter(
             id_field=cfg.data.id_col,
             k=cfg.cv.k,
